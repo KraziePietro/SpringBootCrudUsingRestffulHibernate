6e25e4da11445f16d23aa6a50bb6112fe39089d0
KraziePietro <Kishorlate74@gmail.com>
2022-08-11 23:53:48 +0530

    SBCWR-3 create service interface

--- src/main/java/com/bikkadIt/SpringBootCrudRestfulHibernate/service/UserServiceI.java
+++ src/main/java/com/bikkadIt/SpringBootCrudRestfulHibernate/service/UserServiceI.java
@@ -2,7 +2,19 @@ package com.bikkadIt.SpringBootCrudRestfulHibernate.service;
 
 import java.util.List;
 
+import com.bikkadIt.SpringBootCrudRestfulHibernate.model.User;
+
 public interface UserServiceI {
 
-
+	public int  saveUserService(User user);
+	
+	public List<User>  getAllUsersService();
+	
+	public  User loginCheck(User user);
+	
+	public User  editUser(int uid);
+	
+	public User update(User user);
+	
+	public User delete(int uid);
 }
--- src/main/java/com/bikkadIt/SpringBootCrudRestfulHibernate/service/UserServiceImpl.java
+++ src/main/java/com/bikkadIt/SpringBootCrudRestfulHibernate/service/UserServiceImpl.java
@@ -1,5 +1,54 @@
 package com.bikkadIt.SpringBootCrudRestfulHibernate.service;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bikkadIt.SpringBootCrudRestfulHibernate.dao.UserDaoI;
+import com.bikkadIt.SpringBootCrudRestfulHibernate.model.User;
+
+public class UserServiceImpl implements UserServiceI{
+
+	@Autowired
+	private UserDaoI userDaoI;
+	
+	@Override
+	public int saveUserService(User user) {
+		System.out.println("Service Layer");
+		System.out.println(user);
+		int id = userDaoI.saveUserDao(user);
+		System.out.println(id);
+		return id;
+	}
+
+	@Override
+	public List<User> getAllUsersService() {
+		List<User> list = userDaoI.getAllUserDao();
+		return list;
+	}
+
+	@Override
+	public User loginCheck(User user) {
+		User user1 = userDaoI.loginCheck(user);
+		return user1;
+	}
+
+	@Override
+	public User editUser(int uid) {
+		User user = userDaoI.editUser(uid);
+		return user;
+	}
+
+	@Override
+	public User update(User user) {
+		User user2 = userDaoI.update(user);
+		return user2;
+	}
+
+	@Override
+	public User delete(int uid) {
+		User user = userDaoI.delete(uid);
+		return user;
+	}
 
 }
