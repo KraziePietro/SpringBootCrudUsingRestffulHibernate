8ff5a1f27ebe429b61f5411085cf24d721a9fbeb
KraziePietro <Kishorlate74@gmail.com>
2022-08-11 12:16:28 +0530

    SBCWR-1: Create Project And Required Package Structure

---
+++ src/test/java/com/bikkadIt/SpringBootCrudRestfulHibernate/SpringBootCrudUsingRestfulHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadIt.SpringBootCrudRestfulHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudUsingRestfulHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
